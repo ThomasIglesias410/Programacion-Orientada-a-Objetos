2d4cf3a020cd09b70529768954bb165db55c7798
ThomasIglesias <thomyiglesias08@gmail.com>
2026-03-26 09:30:53 -0300

    Add files via upload

---
+++ ThomasIglesias-Act 4/ejercicio 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0;
+            int num = 11;
+            while (cont <= 24)
+            {
+                Console.WriteLine(num);
+                num = num + 11;
+                cont = cont + 1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ThomasIglesias-Act 4/ejercicio 3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*3. Realizar un programa que acumule (sume) valores ingresados por teclado hasta
+ingresar el 9999 (no sumar dicho valor, indica que ha finalizado la carga). Imprimir el
+valor acumulado e informar si dicho valor es cero, mayor a cero o menor a cero.*/
+            int num;
+            int acum = 0;
+           
+            string linea;
+            Console.Write("ingrese un número: ");
+            linea = Console.ReadLine();
+            num = int.Parse(linea);
+            while (num != 9999) {
+
+                acum = acum + num;
+
+                Console.Write("ingrese un número: ");
+                linea = Console.ReadLine();
+                num = int.Parse(linea);
+
+
+            }
+
+            Console.WriteLine("total= "+acum);
+            Console.ReadKey();
+        }
+    }
+}
