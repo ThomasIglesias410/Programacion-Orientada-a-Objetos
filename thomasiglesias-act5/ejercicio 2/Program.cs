96b800700add3b4842574a95b63d970624a85e5d
ThomasIglesias <thomyiglesias08@gmail.com>
2026-03-27 09:05:29 -0300

    Add files via upload

---
+++ thomasiglesias-act5/ejercicio 2/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*2. Realizar un programa que lea los lados de n triángulos, e informar:
+a) De cada uno de ellos, qué tipo de triángulo es: equilátero (tres lados
+iguales), isósceles (dos lados iguales), o escaleno (ningún lado igual)
+b) Cantidad de triángulos de cada tipo.
+c) Tipo de triángulo que posee menor cantidad.*/
+            int num,lado1,lado2,lado3;
+            int esc = 0;
+            int iso = 0;
+            int equ = 0;
+            Console.Write("ingrese número de triangulos: ");
+            string linea = Console.ReadLine();
+            num = int.Parse(linea);
+            for (int i = 0; i < num; i++)
+            {
+
+
+                    Console.Write("ingrese lado de triangulo: ");
+                    linea = Console.ReadLine();
+                    lado1 = int.Parse(linea);
+
+                    Console.Write("ingrese lado de triangulo: ");
+                    linea = Console.ReadLine();
+                    lado2 = int.Parse(linea);
+
+                    Console.Write("ingrese lado de triangulo: ");
+                    linea = Console.ReadLine();
+                    lado3 = int.Parse(linea);
+                    if (lado1 == lado2 && lado1 == lado3)
+                    {
+
+                        Console.WriteLine("este triangulo es EQUILATERO");
+                        equ++;
+
+                    }
+
+                    else
+                    {
+
+                        if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+                        {
+                            Console.WriteLine("este triangulo es ISOSCELES");
+                            iso++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("este triangulo es ESCALENO");
+                            esc++;
+                        }
+                    }
+                
+                Console.WriteLine("EQUILATEROS: " + equ);
+                Console.WriteLine("ISOCELES: " + iso);
+                Console.WriteLine("ESCALENOS: " + esc);
+                if (equ < iso && equ < esc)
+                {
+                    Console.WriteLine("los EQUILATEROS tienen la menor cantidad");
+                }
+                else 
+                {
+
+                    if (iso < equ && iso < esc)
+                    {
+                        Console.WriteLine("los ISOCELES tienen la menor cantidad");
+                    }
+                    else
+                    { Console.WriteLine("los ESCALENOS tienen la menor cantidad"); }
+            
+            }
+            Console.ReadKey();
+            }
+        }
+    }
+}
+
---
+++ thomasiglesias-act5/ejercicio 3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Se realiza la carga de 10 valores enteros por teclado. Se desea conocer:
+a) La cantidad de valores ingresados negativos.
+b) La cantidad de valores ingresados positivos.
+c) La cantidad de múltiplos de 15.
+d) El valor acumulado de los números ingresados que son pares.*/
+
+            int n;
+            int neg = 0;
+            int pos = 0;
+            int mul = 0;
+            int par = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("ingrese un número: ");
+                string linea = Console.ReadLine();
+                n = int.Parse(linea);
+                if (n > 0) { pos++; }
+                if (n < 0) { neg++; }
+                if (n%15 == 0) { mul++; }
+                if (n%2 == 0) { par=par+n; }
+
+            }
+        }
+    }
+}
