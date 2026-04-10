5531fa8f2b04e745d08663aaf94dd357b8e21540
ThomasIglesias <thomyiglesias08@gmail.com>
2026-04-10 09:16:40 -0300

    Add files via upload

---
+++ ThomasIglesias-Act 4/ejercicio 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_2
+{
+    internal class Program
+    {
+        /*2. Realizar un programa que imprima 25 términos de la serie 11 - 22 - 33 - 44, etc.
+(No se ingresan valores por teclado)*/
+        static void Main(string[] args)
+        {
+            int cont = 0;
+            int num = 11;
+            while (cont <= 24)
+            {
+                Console.WriteLine(num);
+                num = num + 11;
+                cont = cont + 1;
+            }
+            Console.ReadKey();
+        }
+    }
+}
