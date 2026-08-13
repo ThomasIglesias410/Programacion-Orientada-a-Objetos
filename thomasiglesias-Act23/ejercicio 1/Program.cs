bc083fce39c0fde3bf5bc1b58b453a0097777f19
ThomasIglesias <thomyiglesias08@gmail.com>
2026-08-13 09:29:44 -0300

    Add files via upload

---
+++ thomasiglesias-Act23/ejercicio 1/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_1
+{
+    /*En un servidor de red, las solicitudes de impresión se procesan dinámicamente en orden de
+llegada.
+ Crear la clase Documento que contenga como atributos privados:
+nombreArchivo (string) y cantidadPaginas (int). Definir sus
+propiedades de solo lectura y un constructor que reciba los parámetros nom
+y pag.
+ Crear la clase ServidorImpresion que administre una lista dinámica de
+documentos (List&lt;Documento&gt;).
+ Métodos en ServidorImpresion:
+1. AgregarDocumento(): Solicitar por teclado los datos de un
+documento y agregarlo al final de la lista utilizando .Add().
+2. ImprimirSiguiente(): Si la lista no está vacía, simular la
+impresión del primer documento de la lista (mostrar sus datos en
+consola) y removerlo de la colección mediante .RemoveAt(0). Si está
+vacía, advertir que no hay trabajos pendientes.
+3. MostrarColaPendiente(): Listar todos los documentos que
+están esperando ser impresos y la cantidad total de páginas
+acumuladas en la cola de espera utilizando .Count.*/
+    class documento
+    {
+        private string nombreArchivo;
+        private int cantidadPaginas;
+
+        public documento(string nom, int num)
+        {
+            nombreArchivo = nom;
+            cantidadPaginas = num;
+        }
+        public string nombre
+        {
+            get { return nombreArchivo; }
+            set { nombreArchivo = value; }
+        }
+        public int cantidad
+        {
+            get { return cantidadPaginas; }
+            set { cantidadPaginas = value; }
+        }
+
+    }
+    class servidorImpresion
+    {
+        private List<documento> lista = new List<documento>();
+
+        public void AgregarDocumento()
+        {
+            string nom;
+            int num;
+            Console.Write("ingresa el nombre del archivo: ");
+            nom = Console.ReadLine();
+            Console.Write("ingresa la cantidad de paginas de ese archivo: ");
+            string linea = Console.ReadLine();
+            num = int.Parse(linea);
+            documento d = new documento(nom, num);
+            lista.Add(d);
+            Console.WriteLine();
+        }
+        public void ImprimirSiguiente()
+        {
+          
+            if (lista[0] != null)
+            {
+                Console.WriteLine(lista[0].nombre);
+                lista.RemoveAt(0);
+                Console.WriteLine("registro eliminado");
+
+            }
+            else { Console.WriteLine("no hay trabajos pendientes."); }
+        }
+        public void MostrarColaPendiente()
+        { 
+            foreach (documento d in lista)
+            {
+                Console.WriteLine(d.nombre + " " + d.cantidad);
+            }
+            int t = lista.Count();
+            int total = 0;
+            for (int a = 0; a < t; a++)
+            {
+                total += lista[0].cantidad;
+            }
+            Console.WriteLine("cantidad total de paginas acumuladas en la cola de espera: " + total);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            servidorImpresion si = new servidorImpresion();
+            si.AgregarDocumento();
+            si.ImprimirSiguiente();
+            si.MostrarColaPendiente();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ thomasiglesias-Act23/ejercicio3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio3
+{
+    /*Un centro de conservación marina registra de forma dinámica las especies de
+mamíferos marinos divisadas en la costa para su posterior análisis estadístico.
+ Crear la clase MonitoreoCostero que contenga como atributo privado
+una lista de cadenas de texto List&lt;string&gt; especiesDetectadas.
+ Métodos en MonitoreoCostero:
+1. CargarAvistamientos(): Solicitar por teclado nombres de
+especies marinas avistadas (ej: &quot;Ballena Franca&quot;, &quot;Lobo Marino&quot;,
+&quot;Delfín&quot;) y agregarlos a la lista utilizando .Add(). La carga finaliza
+cuando el usuario ingresa la palabra &quot;FIN&quot;.
+2. MostrarReporteOrdenado(): Imprimir la lista de avistamientos
+organizada alfabéticamente de la A a la Z utilizando el método
+.Sort().
+3. MostrarReporteInvertido(): Imprimir la lista organizada de la
+Z a la A combinando .Sort() con el método .Reverse().
+4. BuscarEspecie(): Pedir al operador que ingrese el nombre de un
+animal y, utilizando los métodos de búsqueda de listas, informar si la
+especie fue divisada en la costa durante el día.*/
+    class MonitoreoCostero
+    {
+        private List<string> especies = new List<string>();
+        public void cargarAvistamientos()
+        {
+            string a;
+            do
+            {
+                Console.Write("ingresa los nombres de las especies avistadas (para finalizar introduzca la palabra ´FIN´): ");
+                a = Console.ReadLine();
+                if(a != "FIN")
+                {
+                    especies.Add(a);
+                }
+            } while (a != "FIN");
+        }
+        public void MostrarReporteOrdenado()
+        {
+            especies.Sort();
+            foreach (string a in especies)
+            {
+                Console.WriteLine(a);
+            }
+        }
+        public void MostrarReporteInvertido()
+        {
+            Console.WriteLine();
+            especies.Sort();
+            especies.Reverse();
+            foreach (string a in especies)
+            {
+                Console.WriteLine(a);
+            }
+        }
+        public void BuscarEspecie()
+        {
+            Console.Write("ingresar nombre de la especie que desea buscar: ");
+            string a = Console.ReadLine();
+            if(especies.Contains(a))
+            {
+                Console.WriteLine("la especie fue avistada");
+            }
+            else { Console.WriteLine("la especie no fue avistada"); }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MonitoreoCostero m = new MonitoreoCostero();
+            m.cargarAvistamientos();
+            m.MostrarReporteOrdenado();
+            m.MostrarReporteInvertido();
+            m.BuscarEspecie();
+            Console.ReadKey();
+        }
+    }
+}
